64ffbff9d87984c0e89720339be8b1dde441ea5b
kane.mx <kane.mx@3850fd93-af4f-0410-be14-a5b7ae43855f>
2009-12-16 14:22:19 +0000

    continue working on export wizard page

--- p2-replication/bundles/org.eclipse.equinox.p2.replication/src/org/eclipse/equinox/p2/replication/internal/wizard/ExportPage.java
+++ p2-replication/bundles/org.eclipse.equinox.p2.replication/src/org/eclipse/equinox/p2/replication/internal/wizard/ExportPage.java
@@ -1,5 +1,6 @@
 package org.eclipse.equinox.p2.replication.internal.wizard;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -10,9 +11,11 @@ import org.eclipse.equinox.p2.replication.Constants;
 import org.eclipse.equinox.p2.replication.P2Replicator;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
@@ -21,12 +24,18 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.Widget;
 import org.osgi.util.tracker.ServiceTracker;
 @SuppressWarnings("restriction")
-public class ExportPage extends WizardPage {
+public class ExportPage extends WizardPage implements Listener{
 
 	private class InstallationContentProvider implements IStructuredContentProvider {
 
@@ -66,6 +75,9 @@ public class ExportPage extends WizardPage {
 
 	private CheckboxTableViewer viewer = null;
 	private P2Replicator replicator = null;
+	private Combo destinationNameField;
+	private Button destinationBrowseButton;
+	private String currentMessage;
 
 	public ExportPage(String pageName) {
 		super(pageName);
@@ -78,7 +90,9 @@ public class ExportPage extends WizardPage {
 		initializeDialogUnits(parent);
 		initializeReplicator();
 		Composite composite = new Composite(parent, SWT.NULL);
-		composite.setLayout(new GridLayout());
+		GridLayout layout = new GridLayout();
+		layout.verticalSpacing = 10;
+		composite.setLayout(layout);
 		composite.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_FILL
 				| GridData.HORIZONTAL_ALIGN_FILL));
 
@@ -89,12 +103,80 @@ public class ExportPage extends WizardPage {
 			label.setText(Message.EXPORTPage_LABEL);
 
 			createInstallationTable(composite);
+			createDestinationGroup(composite);
 		}
+
+		// can not finish initially, but don't want to start with an error
+		// message either
+		if (!(validDestination() && validateOptionsGroup())) {
+			setPageComplete(false);
+		}
+
 		setControl(composite);
 
 		giveFocusToDestination();
 		Dialog.applyDialogFont(composite);
 	}
+
+	private void createDestinationGroup(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		composite.setLayout(layout);
+		composite.setLayoutData(new GridData(
+				GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_FILL));
+
+		Label label = new Label(composite, SWT.NONE);
+		label.setText(Message.ExportPage_LABEL_EXPORTFILE);
+
+		destinationNameField = new Combo(composite, SWT.SINGLE | SWT.BORDER);
+		GridData data = new GridData(GridData.HORIZONTAL_ALIGN_FILL
+				| GridData.GRAB_HORIZONTAL);
+		destinationNameField.setLayoutData(data);
+		destinationNameField.addListener(SWT.Modify | SWT.Selection, this);
+		destinationBrowseButton = new Button(composite, SWT.PUSH);
+		destinationBrowseButton.setText(Message.ExportPage_BUTTON_BROWSER);
+		destinationBrowseButton.addListener(SWT.Selection, this);
+		destinationBrowseButton.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+
+		new Label(parent, SWT.NONE); // vertical spacer
+	}
+
+	/**
+	 * Open an appropriate destination browser so that the user can specify a
+	 * source to import from
+	 */
+	protected void handleDestinationBrowseButtonPressed() {
+		FileDialog dialog = new FileDialog(getContainer().getShell(),
+				SWT.SAVE | SWT.SHEET);
+		dialog.setText(getFileDialogTitle());
+		dialog.setFilterPath(getDestinationValue());
+		dialog.setFilterExtensions(new String[] {Message.EXTENSION_P2F, Message.EXTENSION_ALL});
+		dialog.setFilterNames(new String[] {Message.EXTENSION_P2F_NAME, Message.EXTENSION_ALL_NAME});
+		String selectedFileName = dialog.open();
+
+		if (selectedFileName != null) {
+			setDestinationValue(selectedFileName);
+		}
+	}
+
+	protected String getFileDialogTitle() {
+		return Message.ExportPage_FILEDIALOG_TITLE;
+	}
+
+	/**
+	 * Answer the contents of self's destination specification widget
+	 * 
+	 * @return java.lang.String
+	 */
+	protected String getDestinationValue() {
+		return destinationNameField.getText().trim();
+	}
+
+	protected void setDestinationValue(String selectedFileName) {
+		destinationNameField.setText(selectedFileName);
+	}
+
 
 	private void initializeReplicator() {
 		ServiceTracker tracker = new ServiceTracker(Platform.getBundle(Constants.Bundle_ID).getBundleContext(), 
@@ -110,11 +192,16 @@ public class ExportPage extends WizardPage {
 
 	private void createInstallationTable(Composite parent) {
 		viewer = CheckboxTableViewer.newCheckList(parent,  SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL | SWT.FULL_SELECTION);
+				| SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
 		createColumns(viewer);
 		viewer.setContentProvider(new InstallationContentProvider());
 		viewer.setLabelProvider(new InstallationLabelProvider());
 		viewer.setInput(sortByName(replicator.getRootIUs()));
+		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+			public void selectionChanged(SelectionChangedEvent event) {
+				updatePageCompletion();
+			}
+		});
 	}
 
 	private IInstallableUnit[] sortByName(IInstallableUnit[] rootIUs) {
@@ -132,7 +219,7 @@ public class ExportPage extends WizardPage {
 
 	private void createColumns(TableViewer viewer2) {
 		String[] titles = { Message.Column_Name, Message.Column_Version, Message.Column_Id};
-		int[] bounds = { 400, 200, 400 };
+		int[] bounds = { 200, 100, 200 };
 
 		for (int i = 0; i < titles.length; i++) {
 			TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
@@ -145,5 +232,79 @@ public class ExportPage extends WizardPage {
 		Table table = viewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(false);
+	}
+
+	public void handleEvent(Event event) {
+		Widget source = event.widget;
+
+		if (source == destinationBrowseButton) {
+			handleDestinationBrowseButtonPressed();
+		}
+
+		updatePageCompletion();
+	}
+
+	/**
+	 * Determine if the page is complete and update the page appropriately.
+	 */
+	protected void updatePageCompletion() {
+		boolean pageComplete = determinePageCompletion();
+		setPageComplete(pageComplete);
+		if (pageComplete) {
+			setMessage(null);
+		}
+	}
+
+	protected boolean determinePageCompletion() {
+		// validate groups in order of priority so error message is the most important one
+		boolean complete = validateDestinationGroup() && validateOptionsGroup();
+
+		// Avoid draw flicker by not clearing the error
+		// message unless all is valid.
+		if (complete) {
+			setErrorMessage(null);
+		} else {
+			setErrorMessage(currentMessage);
+		}
+
+		return complete;
+	}
+
+	protected boolean validateOptionsGroup() {
+		if(viewer.getCheckedElements().length > 0)
+			return true;
+
+		currentMessage = getNoOptionsMessage();
+
+		return false;
+	}
+
+
+	protected String getNoOptionsMessage() {
+		return Message.ExportPage_NOINSTALLTION_ERROR;
+	}
+
+
+	/**
+	 * Validate the destination group.
+	 * @return <code>true</code> if the group is valid. If
+	 * not set the error message and return <code>false</code>.
+	 */
+	protected boolean validateDestinationGroup() {
+		if (!validDestination()) {
+			currentMessage = getInvalidDestinationMessage();
+			return false;
+		}
+
+		return true;
+	}
+
+	protected boolean validDestination() {
+		File file = new File(getDestinationValue());
+		return !(file.getPath().length() <= 0 || file.isDirectory());
+	}
+
+	protected String getInvalidDestinationMessage() {
+		return Message.ExportPage_DEST_ERRORMESSAGE;
 	}
 }
--- p2-replication/bundles/org.eclipse.equinox.p2.replication/src/org/eclipse/equinox/p2/replication/internal/wizard/Message.java
+++ p2-replication/bundles/org.eclipse.equinox.p2.replication/src/org/eclipse/equinox/p2/replication/internal/wizard/Message.java
@@ -9,7 +9,17 @@ public class Message extends NLS {
 	public static String EXPORTPage_TITLE;
 	public static String EXPORTPage_DESCRIPTION;
 	public static String EXPORTPage_LABEL;
+	public static String ExportPage_BUTTON_BROWSER;
+	public static String ExportPage_DEST_ERRORMESSAGE;
 	public static String ExportPage_ERROR_CONFIG;
+	public static String ExportPage_FILEDIALOG_TITLE;
+	public static String EXTENSION_ALL;
+	public static String EXTENSION_ALL_NAME;
+	public static String EXTENSION_P2F_NAME;
+	public static String EXTENSION_NAME;
+	public static String EXTENSION_P2F;
+	public static String ExportPage_LABEL_EXPORTFILE;
+	public static String ExportPage_NOINSTALLTION_ERROR;
 
 	static {
 		NLS.initializeMessages(
