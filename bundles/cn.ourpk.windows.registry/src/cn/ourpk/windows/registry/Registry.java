21dd68846b180ba8296c9bf0c789c2e95e54290d
kane.mx <kane.mx@3850fd93-af4f-0410-be14-a5b7ae43855f>
2011-02-25 14:30:49 +0000

    tag 1.2 version of p2 replication

--- bundles/cn.ourpk.windows.registry/src/cn/ourpk/windows/registry/Registry.java
+++
@@ -1,7 +0,0 @@
-package cn.ourpk.windows.registry;
-
-public final class Registry {
-	static{
-		System.loadLibrary("registry"); //$NON-NLS-1$
-	}
-}
--- robot/eclipse/plugins/cn.ourpk.robot/src/cn/ourpk/robot/PostProvider.java
+++
@@ -1,8 +0,0 @@
-package cn.ourpk.robot;
-
-import java.util.Date;
-
-public interface PostProvider {
-	void doLogin();
-	void post(String title, String content, Date pubDate);
-}
