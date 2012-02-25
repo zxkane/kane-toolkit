23256800297c8e417ad8236b5685bf9a74f985cc
kane.mx <kane.mx@3850fd93-af4f-0410-be14-a5b7ae43855f>
2012-02-25 06:15:56 +0000

    initial code base.

---
+++ tools/Encypt/src/EncyptPropFile.java
@@ -0,0 +1,54 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class EncyptPropFile {
+
+	/**
+	 * @param args
+	 * @throws NoSuchPaddingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IOException {
+		byte[] aeskey = args[0].getBytes(); //$NON-NLS-1$
+		System.out.println(args[1]);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		SecretKeySpec keyspec = new SecretKeySpec(aeskey, "AES"); //$NON-NLS-1$
+		Cipher cipher = Cipher.getInstance("AES"); //$NON-NLS-1$
+		cipher.init(Cipher.ENCRYPT_MODE, keyspec);
+		CipherOutputStream aesOut = new CipherOutputStream(out, cipher);
+		aesOut.write(args[1].getBytes());
+		aesOut.close();
+		
+		FileOutputStream fileout = new FileOutputStream("token");
+		out.writeTo(fileout);
+		fileout.close();
+		
+		byte[] encyptedBytes = out.toByteArray();
+		System.out.println("Encypted: " + new String(encyptedBytes));
+		ByteArrayInputStream input = new ByteArrayInputStream(encyptedBytes);
+		keyspec = new SecretKeySpec(aeskey, "AES"); //$NON-NLS-1$
+		cipher = Cipher.getInstance("AES"); //$NON-NLS-1$
+		cipher.init(Cipher.DECRYPT_MODE, keyspec);
+		CipherInputStream aesIn = new CipherInputStream(input, cipher);
+		Properties props = new Properties();
+		props.load(aesIn);
+		for (Object key : props.keySet()) {
+			System.out.println(key + "=" + props.get(key));
+		}
+	}
+}
